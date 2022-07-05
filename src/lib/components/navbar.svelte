534369f1be5d3dc4c843e1fc8286edaaab469a51
BrandonVerzuu <brandon.verzuu@appythings.com>
2022-07-05 22:07:34 +0200

    Fixed hidden menu items on small screens

--- src/lib/components/navbar.svelte
+++ src/lib/components/navbar.svelte
@@ -2,7 +2,7 @@
 	<div class="navbar-start">
 		<a href="/" class="btn btn-ghost normal-case text-xl">Home</a>
 	</div>
-	<div class="navbar-center hidden lg:flex">
+	<div class="navbar-center lg:flex">
 		<ul class="menu menu-horizontal p-0">
 			<li><a href="/blog">Blog</a></li>
 			<li><a href="/about">Over</a></li>
