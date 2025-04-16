a2603a0a211c363ff22c312bf1209ec46ebf148b
brand0new <brandonverzuu@gmail.com>
2025-04-16 23:12:54 +0200

    fix: Tweaked some config for theming

--- app.config.ts
+++ app.config.ts
@@ -1,13 +1,9 @@
 export default defineAppConfig({
   ui: {
-    primary: "teal",
-    gray: "neutral",
-    formGroup: {
-      help: "text-xs mt-1 text-gray-500 dark:text-gray-400",
-      error: "text-xs mt-1 text-red-500 dark:text-red-400",
-      label: {
-        base: "text-sm block font-medium text-gray-500 dark:text-gray-200",
-      },
+    colors: {
+      // Determine Color Scheme for personal site
+      primary: "teal",
+      secondary: "rose",
     },
     button: {
       rounded:
